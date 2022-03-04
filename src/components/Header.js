0b2c97a2042570c49fcf593b0915d11d3960cbbb
Sabrina Hanson <sabhanson7@gmail.com>
2022-03-03 16:27:32 -0800

    OMG i finally understand useState. working on contact form and checking whether input is focused or not

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import Navigation from './Navigation'
+import Navigation from "./Navigation";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
-import Contact from './pages/Contact'
+import Contact from "./pages/Contact";
 import Resume from "./pages/Resume";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -10,51 +10,61 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const styles = {
   div: {
     backgroundImage: `url('./pearl-lake.jpg')`,
-    width: '100%',
-    display: 'flex',
-    height: '200px',
-    border: '3px dotted #ffa500',
-    borderRadius: '20px',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "100%",
+    paddingTop: '30px',
+    // display: "flex",
+    height: "200px",
+    border: "3px dotted #ffa500",
+    borderRadius: "20px",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  innerDiv: {
+    width: '100%'
   },
   h1: {
     fontSize: "3rem",
     color: "black",
     fontFamily: "Unica One, cursive ",
     textShadow: "1px 1px 2px #FFA500",
-  }
-}
+  },
+  p: {
+    fontSize: "2rem",
+  },
+};
 
 function Header() {
-
   const [currentPage, setCurrentPage] = useState("About");
 
   const renderPage = () => {
     if (currentPage === "About") {
-        return <About />
+      return <About />;
     }
     if (currentPage === "Portfolio") {
-        return <Portfolio />
+      return <Portfolio />;
     }
     if (currentPage === "Contact") {
-        return <Contact />
+      return <Contact />;
     }
     if (currentPage === "Resume") {
-        return <Resume />
+      return <Resume />;
     }
-}
-
+  };
 
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <div>
-    <div style={styles.div}>
-      <h1 style={styles.h1}>SABRINA HANSON</h1>
-      <p>jr web developer</p>
-    </div>
-      < Navigation currentPage={currentPage} handlePageChange={handlePageChange}/>
+      <div style={styles.div}>
+        <h1 style={styles.h1}>SABRINA HANSON</h1>
+        <div style={styles.innerDiv}>
+          <p>jr web developer</p>
+        </div>
+      </div>
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
       {renderPage()}
     </div>
   );
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import '../../styles/Contact.css'
 
 const styles = {
   div: {
-    padding: '2rem 0 3rem 0'
+    padding: "2rem 0 3rem 0",
   }
-}
+};
 
 // this page is loaded when CONTACT is selected from the navbar
-    // includes a form with fields for NAME, EMAIL, and MESSAGE
-    // when the form is unfocused, the validations are run to check if things were filled out properly
-    // validates that name is filled out
-    // validates that email is proper format
-    // validates that a message is entered
-
+// includes a form with fields for NAME, EMAIL, and MESSAGE
+// when the form is unfocused, the validations are run to check if things were filled out properly
+// validates that name is filled out
+// validates that email is proper format
+// validates that a message is entered
 
 export default function Contact() {
+  const [focus, setFocus] = useState("Unfocus");
+
+  const handleFocusChange = (hover) => setFocus(hover);
+
   return (
     <div style={styles.div}>
       <h1>Contact Me!</h1>
       <form>
-        {/* <label>Your Name</label> */}
-        <input type={"text"} placeholder={"enter your name..."}></input><br/>
+        {/* NAME INPUT */}
+        {/* onFocus (when interacting) set the focus state to focus, onBlur (when not interacting) run the validation to check if the name is in the correct format */}
+        <input
+          onFocus={() => handleFocusChange("Focus")}
+          onBlur={() => handleFocusChange("Unfocus") }
+          
+          className={focus === "Unfocus" ? "unfocus" : "focus"}
+          type={"text"}
+          placeholder={"enter your name..."}
+        ></input>
+        <br />
         {/* <label>Your Email</label> */}
-        <input type={"text"} placeholder={"enter your email..."}></input><br/>
+        <input type={"text"} placeholder={"enter your email..."}></input>
+        <br />
         {/* <label>Your Message</label> */}
-        <textarea type={"text"} placeholder={"enter your message..."}></textarea><br />
+        <textarea
+          type={"text"}
+          placeholder={"enter your message..."}
+        ></textarea>
+        <br />
         <input type={"submit"}></input>
       </form>
     </div>
   );
 }
+
+// onFocus
