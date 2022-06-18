98339114bff74aa0eee80f656c95ddb8f47687fd
Sabrina Hanson <sabhanson7@gmail.com>
2022-06-18 14:53:51 -0700

    Updates

---
+++ precache-manifest.1c5b079ece91de531f2118ceb62770af.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "834f3f51e0a7fb84ef72",
+    "url": "/sab-react-portfolio/static/css/main.d833eaec.chunk.css"
+  },
+  {
+    "revision": "834f3f51e0a7fb84ef72",
+    "url": "/sab-react-portfolio/static/js/main.834f3f51.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/sab-react-portfolio/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "defc74babea3f13d272d",
+    "url": "/sab-react-portfolio/static/css/2.1b7191b5.chunk.css"
+  },
+  {
+    "revision": "defc74babea3f13d272d",
+    "url": "/sab-react-portfolio/static/js/2.defc74ba.chunk.js"
+  },
+  {
+    "revision": "b8584d87d92571d764d2",
+    "url": "/sab-react-portfolio/static/js/runtime~main.b8584d87.js"
+  },
+  {
+    "revision": "45a045ff83f32e59c6f7ac629d775445",
+    "url": "/sab-react-portfolio/static/media/IMG_3283.45a045ff.jpg"
+  },
+  {
+    "revision": "d0c12ee1ed6118c2352fd6fc2d3689ca",
+    "url": "/sab-react-portfolio/static/media/Resume.d0c12ee1.pdf"
+  },
+  {
+    "revision": "0493a295157e548ba9a188f9cad2eae2",
+    "url": "/sab-react-portfolio/index.html"
+  }
+];
