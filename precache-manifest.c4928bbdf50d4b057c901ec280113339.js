df69725838b08ccbd0afd52249e91aaa73907d6f
Sabrina Hanson <sabhanson7@gmail.com>
2022-11-22 10:11:22 -0800

    Updates

--- precache-manifest.c4928bbdf50d4b057c901ec280113339.js
+++ precache-manifest.c4928bbdf50d4b057c901ec280113339.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ad6647735137a0c901b9",
+    "revision": "9147be53200713ea2778",
     "url": "/sab-react-portfolio/static/css/main.0e47ab43.chunk.css"
   },
   {
-    "revision": "ad6647735137a0c901b9",
-    "url": "/sab-react-portfolio/static/js/main.ad664773.chunk.js"
+    "revision": "9147be53200713ea2778",
+    "url": "/sab-react-portfolio/static/js/main.9147be53.chunk.js"
   },
   {
     "revision": "6a7bfa4df9242b40edff",
@@ -32,7 +32,7 @@ self.__precacheManifest = [
     "url": "/sab-react-portfolio/static/media/Resume.9dc3ca20.pdf"
   },
   {
-    "revision": "50d39ce586303bbb4dbe7b2a9cf87a50",
+    "revision": "44f709d06b0f698703af3345241107eb",
     "url": "/sab-react-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sab-react-portfolio/precache-manifest.18a2d998889748b91840bd6e2f9593e8.js"
+  "/sab-react-portfolio/precache-manifest.c4928bbdf50d4b057c901ec280113339.js"
 );
 
 workbox.clientsClaim();
